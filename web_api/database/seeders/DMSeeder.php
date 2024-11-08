ddf47ed486320e39f3d8dcc769c8670ffa8452c2
sajithapislk <sajith@apis.lk>
2024-11-08 09:38:37 +0530

    app working

--- web_api/database/seeders/DMSeeder.php
+++ web_api/database/seeders/DMSeeder.php
@@ -22,21 +22,7 @@ class DMSeeder extends Seeder
                 'area_id' => '1',
                 'tp'=>'1234567890',
             ],
-            [
-                'name' => 'Sajith',
-                'email' => 'dm@test.com',
-                'password' => Hash::make('12345678'),
-                'area_id' => '2',
-                'tp'=>'713596504',
-            ],
-            [
-                'name' => 'Saheer',
-                'email' => 'dm@test.com',
-                'password' => Hash::make('12345678'),
-                'area_id' => '3',
-                'tp'=>'734509675',
-            ]
-           
+
         ]);
     }
 }
